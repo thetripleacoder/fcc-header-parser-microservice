d1330656bf7cbf486bd29e5f8b0d0f3df6ef1a74
aldousalde <85655422+thetripleacoder@users.noreply.github.com>
2023-11-14 05:01:23 +0800

    Fix Vercel migration: - Export the Express API - Add vercel.json configuration  source: https://shadowsmith.com/how-to-deploy-an-express-api-to-vercel

--- index.js
+++ index.js
@@ -37,3 +37,6 @@ app.get('/api/whoami', function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+//  Export the Express API
+module.exports = app;
